c07ab385323518b60b454d616ba255cb9722aed0
KonstantinUm1993 <112888972+KonstantinUm1993@users.noreply.github.com>
2023-09-18 19:39:48 +0300

    Version 1.0

---
+++ main.py
@@ -0,0 +1,24 @@
+import sqlite3
+from flask import Flask, render_template
+
+connection = sqlite3.connect("tovary.db", check_same_thread=False)
+cursor = connection.cursor()
+
+app = Flask(__name__)
+
+@app.route("/")
+def site():
+    return render_template("main_page.html")
+
+
+@app.route("/tovary")
+def assort():
+    cursor.execute("select * from Laptops")
+    result = cursor.fetchall()
+    return render_template("goods_page.html", laptops=result)   
+
+
+if __name__ ==  "__main__":
+    app.run(debug=True, port = 8000)
+
+
