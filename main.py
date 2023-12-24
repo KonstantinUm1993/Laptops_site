4ae813e60bc5c720a89b1b74a37bdb76f4fd3bb0
KonstantinUm1993 <112888972+KonstantinUm1993@users.noreply.github.com>
2023-12-24 11:07:33 +0300

    Ver from 24.12.23
    Добавили фидбэк раздел с отправкой на почту

--- main.py
+++ main.py
@@ -1,11 +1,14 @@
 import sqlite3
+import smtplib
 from flask import Flask, render_template, request
 
 
-connection = sqlite3.connect(r"Laptops_site/tovary.db", check_same_thread=False)
+connection = sqlite3.connect("tovary.db", check_same_thread=False)
 cursor = connection.cursor()
 
 app = Flask(__name__)
+smtpObj = smtplib.SMTP('smtp.gmail.com', 587)
+
 
 @app.route("/", methods=["POST","GET"])
 def site():
@@ -77,7 +80,20 @@ def assort_2():
         cursor.execute("select count(ID) from Laptops")
         len_of_id = cursor.fetchall()
         numb_of_goods = len_of_id[0][0]
-        return render_template("goods_page.html", laptops=result, page=page, numb_of_goods=numb_of_goods)   
+        return render_template("goods_page.html", laptops=result, page=page, numb_of_goods=numb_of_goods)
+
+@app.route("/feedback", methods=["POST","GET"])
+def feedback():
+    if request.method=="POST":
+        email=request.form["email"]
+        text=request.form["text"]
+        smtpObj.starttls()
+        smtpObj.login('laskin.danil228@gmail.com','erok poih qdqz udfp')
+        smtpObj.sendmail("laskin.danil228@gmail.com",email,text)
+        smtpObj.quit()
+        return render_template("feedback.html")
+    else:
+        return render_template("feedback.html")
 
 if __name__ ==  "__main__":
     app.run(debug=True, port = 8000)
